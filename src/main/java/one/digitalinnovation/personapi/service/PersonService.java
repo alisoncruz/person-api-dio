a0459bc6f3e683fd42fe76fe1a7b5731431eb4df
alisoncruz <alisonn83@gmail.com>
2021-06-08 11:43:04 -0300

    add list function

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
@@ -23,5 +26,10 @@ public class PersonService {
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Person created with ID: "+ savedPerson.getId()).build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(person -> personMapper.toDTO(person)).collect(Collectors.toList());
     }
 }
